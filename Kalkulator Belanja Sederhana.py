c11c9c28bce59b5d59ff818bf3e804a8b9e68628
Martina Ekya Rahmat <martinaekya10@gmail.com>
2026-08-26 09:27:38 +0700

    Add files via upload

---
+++ Kalkulator Belanja Sederhana.py
@@ -0,0 +1,5 @@
+nama_barang = input("Masukkan nama barang: ")
+harga_barang = int(input("Masukkan harga barang: "))
+jumlah_barang = int(input("Masukkan jumlah barang: "))
+total = harga_barang * jumlah_barang
+print(f"Kamu membeli {nama_barang} sebanyak {jumlah_barang} dengan harga Rp. {total}.")
